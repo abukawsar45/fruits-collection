d2b9b0bfd23d025e318de5ec1564b7a1b7a27c7a
abukawsar45 <kawsar1146236@gmail.com>
2023-11-09 19:49:20 +0600

    edit in menu page

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 const MenuPage = () => {
   return (
-    <div className='md:mt-3 md:mx-[17px] bg-white h-screen mt-[60px] md:order-2 rounded-lg flex flex-col justify-center items-center '>
+    <div className='md:mt-3 md:mx-[17px] bg-white h-screen pt-[60px] md:order-2 rounded-lg flex flex-col md:justify-center items-center '>
       <Image src={emptyImage} alt='empty-image' />
-      <h3 className='mt-4 md:mt-10 text-xl md:text-3xl font-semibold md:font-bold'>
+      <h3 className='mt-4 md:mt-10 w-36 md:w-auto h-auto text-xl md:text-3xl font-semibold md:font-bold'>
         This page is empty
       </h3>
     </div>
